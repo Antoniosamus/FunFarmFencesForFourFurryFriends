e13b2646486756576b0d8902d05eaa773417c6ff
fibriZo <fibrizo.raziel@gmail.com>
2015-01-23 23:08:02 +0100

    + IRouteTracer

---
+++ Assets/Scripts/Input/IRouteTracer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IRouteTracer
+{
+  event Action<Vector3> OnRouteStart;
+  event Action<Queue<Vector3>> OnRouteStop;
+  event Action<Vector3> OnRouteStay;
+  event Action OnRouteCancel;
+
+  Queue<Vector3> LastRoute { get; }
+}
+
+

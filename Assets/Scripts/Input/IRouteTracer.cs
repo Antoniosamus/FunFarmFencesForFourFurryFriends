c829f46717a3f8d197e4d67a9b70e2cf4c8f87c1
fibriZo <fibrizo.raziel@gmail.com>
2015-01-23 23:27:05 +0100

    [IRouteTracer] +Comments

--- Assets/Scripts/Input/IRouteTracer.cs
+++ Assets/Scripts/Input/IRouteTracer.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public interface IRouteTracer
 {
-  event Action<Vector3> OnRouteStart;
-  event Action<Queue<Vector3>> OnRouteStop;
-  event Action<Vector3> OnRouteStay;
-  event Action OnRouteCancel;
+  event Action<Vector3> OnRouteStart;         // Comienzo de trazado -> envía posición inicial
+  event Action<Queue<Vector3>> OnRouteStop;   // Fin de trazado     -> envía la ruta trazada
+  event Action<Vector3> OnRouteStay;          // Durante el trazado -> envían posiciones actuales
+  event Action OnRouteCancel;                 // Cancelación de la ruta
 
-  Queue<Vector3> LastRoute { get; }
+  Queue<Vector3> LastRoute { get; } // Ruta trazada
 }
 
 

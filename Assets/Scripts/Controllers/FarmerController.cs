fd885d4acc075279d4d81a510ee34ffb058f0c6c
Francisco José de la Torre <xitotix@gmail.com>
2015-01-24 14:29:31 +0100

    Runner modified WIP

--- Assets/Scripts/Controllers/FarmerController.cs
+++ Assets/Scripts/Controllers/FarmerController.cs
@@ -2,31 +2,93 @@
 using System.Collections;
 using System.Collections.Generic;
 
+[RequireComponent(typeof(Runner))]
+
 public class FarmerController : MonoBehaviour {
 
-	[SerializeField] private Queue<Vector3> route;
 
-	IRouteTracer tracer; 
+	[SerializeField] List<GameObject> points;
+	[SerializeField] private Queue<Vector2> route;
+	//IRouteTracer tracer; 
+	Runner runner;
+
+	public delegate void CollideWithObstacle(Collision2D collision);
+	public event CollideWithObstacle OnCollideWithObstacle;
+
+	public delegate void CollideWithWayPoint(Collision2D collision);
+	public event CollideWithWayPoint OnCollideWithWayPoint;
+
+	[SerializeField] GameObject pointPrefab;
+
+	public void Awake()
+	{
+		if (runner == null) 
+		{
+			runner = GetComponent<Runner>();
+		}
+
+		route = new Queue<Vector2> ();
+		foreach (GameObject vector in points) 
+		{
+			//Instantiate(pointPrefab, vector, Quaternion.identity);
+			Vector2 vectorAux = (Vector2)vector.transform.position;
+			route.Enqueue(vectorAux);
+		}
+	}
 
 	public void OnEnable()
 	{
-		tracer.OnRouteStart += HandleOnRouteStart;
-	}
-	
-	public void OnDisable()
-	{
-		tracer.OnRouteStart -= HandleOnRouteStart;
+		//tracer.OnRouteStart += HandleOnRouteStart;
+		runner.OnCollisionAppears += OnRunnerCollision;
+		FollowRoute ();
 	}
 
+	public void OnDisable()
+	{
+		//tracer.OnRouteStart -= HandleOnRouteStart;
+		runner.OnCollisionAppears -= OnRunnerCollision;
+	}
 	
 	public void FollowRoute ()
 	{
-		
+		Debug.Log ("Hello");
+
+		GoToNextPoint (route.Dequeue());
 	}
 
-	void HandleOnRouteStart (Vector3 obj)
+	public void GoToNextPoint(Vector3 point)
 	{
-		
+		runner.GoToNextPoint (point);
+	}
+
+	private void OnRunnerCollision (Collision2D collision)
+	{
+		string collisionName = LayerMask.LayerToName (collision.collider.gameObject.layer);
+
+		if (collisionName == "Obstacle") 
+		{
+			if (OnCollideWithObstacle != null)
+			{
+				OnCollideWithObstacle(collision);
+			}
+		}
+		else if (collisionName == "Waypoint")
+			{
+				FollowRoute();
+
+				if (OnCollideWithWayPoint != null)
+				{
+					OnCollideWithWayPoint(collision);
+				}
+			}
+
+
+		route.Clear ();
+	}
+
+	private void HandleOnRouteStart (Vector3 obj)
+	{
+		FollowRoute ();
 	}
 
 
---
+++ Assets/Scripts/Controllers/Runner.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
+
+public class Runner : MonoBehaviour{
+
+	Rigidbody2D rigidbody;
+	BoxCollider2D collider;
+	//[SerializeField] Vector2 velocity;
+	
+	public delegate void OnCollision(Collision2D collision);
+	public event OnCollision OnCollisionAppears;
+
+
+	public void OnEnable()
+	{
+		if (rigidbody == null) 
+		{
+			rigidbody = GetComponent<Rigidbody2D>();
+		}
+	}
+
+	public void GoToNextPoint (Vector3 point)
+	{
+		transform.LookAt(point);
+		Walk (point);
+	}
+	
+	void Walk (Vector3 point)
+	{
+		Vector3 vel = this.transform.right;
+
+		Debug.Log (this.transform.TransformDirection (vel));
+
+		rigidbody.velocity = (Vector2)transform.forward;
+
+
+	}
+
+	void FixedUpdate()
+	{
+		rigidbody.velocity = transform.forward;
+	}
+
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		Stop (collision);
+	}
+
+	private void Stop (Collision2D collision)
+	{
+		rigidbody.velocity = Vector2.zero;
+
+		if (OnCollisionAppears != null) 
+		{
+			OnCollisionAppears(collision);
+		}
+	}
+}

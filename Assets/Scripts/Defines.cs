a399b322d21625e651aa79f8c2a105925f622b34
alejandro carballo <acarball@bitbucket.org>
2015-01-24 22:01:01 +0100

    Integrando XMLParser.cs con GameManager

---
+++ Assets/Scripts/Defines.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Defines : MonoBehaviour {
+    public static string NodeLevelData = "LevelsData/LevelData";
+    public static string NodePlayerData = "LevelsData/LevelData/Player";
+    public static string NodeFarmerData = "LevelsData/LevelData/Farmers";
+    public static string NodeAnimalData = "LevelsData/LevelData/Animal";
+    public static string NodeObstacleData = "LevelsData/LevelData/Obstacle";
+    public static string Id = "id";
+    public static string Score = "score";
+    public static string FormatFile = ".xml";
+    public static string Name = "name";
+    public static string Amount = "Amount";
+    public static string Path = "";
+
+
+}
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : Singleton<GameManager> {
+
+    private XMLParser LevelFile;
+    private List<XMLParser.LevelData> lLevelData;
+    
 
 	// Use this for initialization
 	void Awake () {
         IAManager.Instance.Inizialize(100);
 	}
+
+    void Start()
+    {
+        lLevelData = LevelFile.GetLevelData();
+        //invocamos al IAMnager con los tipos de animales y cantidad de cada uno
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+
 }

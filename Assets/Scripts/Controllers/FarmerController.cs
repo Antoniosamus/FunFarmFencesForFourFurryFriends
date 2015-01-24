ae0925c6ec7e1bc9b652329b2d85837dff93ecfc
Francisco José de la Torre <xitotix@gmail.com>
2015-01-24 09:48:02 +0100

    Adding farmer controller WIP

---
+++ Assets/Scripts/Controllers/FarmerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FarmerController : MonoBehaviour {
+
+	[SerializeField] private Queue<Vector3> route;
+
+	IRouteTracer tracer; 
+
+	public void OnEnable()
+	{
+		tracer.OnRouteStart += HandleOnRouteStart;
+	}
+	
+	public void OnDisable()
+	{
+		tracer.OnRouteStart -= HandleOnRouteStart;
+	}
+
+	
+	public void FollowRoute ()
+	{
+		
+	}
+
+	void HandleOnRouteStart (Vector3 obj)
+	{
+		
+	}
+
+
+
+
+}
